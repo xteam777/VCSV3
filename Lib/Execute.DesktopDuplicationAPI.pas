f23d45833a79da2bc1389536aa6ea8d31d4f82cb
xteam777 <38719485+xteam777@users.noreply.github.com>
2023-09-19 11:19:08 -0700

    1

--- Lib/Execute.DesktopDuplicationAPI.pas
+++ Lib/Execute.DesktopDuplicationAPI.pas
@@ -53,7 +53,6 @@ type
     function CreateDD : Boolean;
     procedure DestroyDD;
     function GetScreenInfoChanged: Boolean;
-    procedure SetClipRect(const Rect : TRect);
   public
     pScreenWidth, pScreenHeight, pBitsPerPixel, pClipRect: Pointer;
     DirtyR, MovedR : array [0..10000] of TRect;
@@ -91,6 +90,8 @@ begin
   New(pClipRect);
 
   inherited;
+
+  FFullScreen := True;
 
   CreateDD;
 end;
@@ -631,18 +632,18 @@ function TDesktopDuplicationWrapper.GetScreenInfoChanged: Boolean;
 var
   NewBitsPerPixel : Integer;
 begin
-  Result := false;
+  Result := False;
 
   if PInteger(pScreenWidth)^ <> Desc.Width then
   begin
     PInteger(pScreenWidth)^ := Desc.Width;
-    Result := true;
+    Result := True;
   end;
 
   if PInteger(pScreenHeight)^ <> Desc.Height then
   begin
     PInteger(pScreenHeight)^ := Desc.Height;
-    Result := true;
+    Result := True;
   end;
 
   if Result and FFullScreen then
@@ -665,21 +666,22 @@ begin
   if PInteger(pBitsPerPixel)^ <> NewBitsPerPixel then
   begin
     PInteger(pBitsPerPixel)^ := NewBitsPerPixel;
-    Result := true;
+    Result := True;
   end;
 end;
 
-procedure TDesktopDuplicationWrapper.SetClipRect(const Rect : TRect);
-begin
-  if (Rect.Width = 0) or (Rect.Height = 0) then
-  begin
-    FullScreen := true;
-    PRect(pClipRect)^ := TRect.Create(0, 0, PInteger(pScreenWidth)^, PInteger(pScreenHeight)^);
-  end else
-  begin
-    FullScreen := false;
-    PRect(pClipRect)^ := Rect;
-  end;
-end;
+//procedure TDesktopDuplicationWrapper.SetClipRect(const Rect : TRect);
+//begin
+//  if (Rect.Width = 0) or (Rect.Height = 0) then
+//  begin
+//    FullScreen := True;
+//    PRect(pClipRect)^ := TRect.Create(0, 0, PInteger(pScreenWidth)^, PInteger(pScreenHeight)^);
+//  end
+//  else
+//  begin
+//    FullScreen := False;
+//    PRect(pClipRect)^ := Rect;
+//  end;
+//end;
 
 end.
